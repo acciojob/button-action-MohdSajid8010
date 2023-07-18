50e00c47f963c5185ac5e49637faa81fd05e69e4
sajid <mohdsajid8010@gmail.com>
2023-07-18 15:13:52 +0530

    commit2

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
+let p_content = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
 const App = (props) => {
-  let [pVisible, setPVisible] = useState("hide")
+  let [content, setContent] = useState("");
   return (
     <div className="App" id="main">
       {/* // Do not alter the main div */}
-      <button id="click" onClick={() => setPVisible("show")}>Show para</button>
-      {
-        pVisible == "hide" ? ("") : (<p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
-      }
+      <button id="click" onClick={() => setContent(p_content)}>Show para</button>
 
+      <p id="para" className="hide">{content}</p>
     </div>
   );
 }
